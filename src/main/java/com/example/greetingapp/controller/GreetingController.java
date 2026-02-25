dad004c7ced6d4b2c22b700d02496620f9275625
Anmol Kumar Ojha <150778576+anmolkr7@users.noreply.github.com>
2026-02-25 14:34:01 +0530

    Change hello endpoint to /helloanmol

--- src/main/java/com/example/greetingapp/controller/GreetingController.java
+++ src/main/java/com/example/greetingapp/controller/GreetingController.java
@@ -22,7 +22,7 @@ public class GreetingController {
         return new Greeting((int) counter.incrementAndGet(), String.format(template, name));
     }
 
-    @GetMapping("/hello")
+    @GetMapping("/helloanmol")
     public String sayHello(){
         return greetingService.sayHello();
     }
